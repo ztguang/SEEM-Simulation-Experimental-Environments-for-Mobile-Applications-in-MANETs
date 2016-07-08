18f4c104823a9d47333ae42e5d1a49d4ad6bc2f1
张同光 <jsjoscpu@163.com>
2016-07-08 10:32:23 +0800

    Update init_install_androic-x86_64_in_QEMU-KVM.sh

--- init_install_androic-x86_64_in_QEMU-KVM.sh
+++ init_install_androic-x86_64_in_QEMU-KVM.sh
@@ -10,11 +10,11 @@ cp out/etc/zebra.conf /system/xbin/quagga/etc/
 cp out/etc/ospf6d.conf /system/xbin/quagga/etc/
 
 cp -r out/sbin/ /system/xbin/quagga/
-cp out/lib/libzebra.so /lib64/
-cp out/lib/libospf.so /lib64/
-cp out/lib/libospfapiclient.so /lib64/
-cp out/lib/libstlport_shared.so /lib64/
-cp out/lib/libc++_shared.so /lib64/
+cp out/lib/libzebra.so /system/lib64/
+cp out/lib/libospf.so /system/lib64/
+cp out/lib/libospfapiclient.so /system/lib64/
+cp out/lib/libstlport_shared.so /system/lib64/
+cp out/lib/libc++_shared.so /system/lib64/
 
 cp out/busybox-x86_64 /system/xbin/
 cp out/init_in_android-x86_64.sh /system/xbin/
