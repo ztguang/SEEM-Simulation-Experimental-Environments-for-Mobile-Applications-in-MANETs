3a5254ccbc50fea529d3dc316c6c38cee607e655
张同光 <jsjoscpu@163.com>
2016-07-08 10:19:24 +0800

    Add files via upload

---
+++ init_install_androic-x86_64_in_QEMU-KVM.sh
@@ -0,0 +1,24 @@
+#!/system/bin/sh
+
+cd /sdcard/tmp
+
+mount -o remount,rw /system
+mount -o remount,rw /
+mkdir -p /system/xbin/quagga/etc
+
+cp out/etc/zebra.conf /system/xbin/quagga/etc/
+cp out/etc/ospf6d.conf /system/xbin/quagga/etc/
+
+cp -r out/sbin/ /system/xbin/quagga/
+cp out/lib/libzebra.so /lib64/
+cp out/lib/libospf.so /lib64/
+cp out/lib/libospfapiclient.so /lib64/
+cp out/lib/libstlport_shared.so /lib64/
+cp out/lib/libc++_shared.so /lib64/
+
+cp out/busybox-x86_64 /system/xbin/
+cp out/init_in_android-x86_64.sh /system/xbin/
+cp out/chrome_51.0.2704.81.apk /sdcard/Download/chrome_51.0.2704.81.apk
+
+chmod 755 /system/xbin/init_in_android-x86_64.sh
+
