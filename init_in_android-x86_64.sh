ea71d56a6e997b109b08bc04c965d8dfdfb5628e
张同光 <jsjoscpu@163.com>
2016-07-12 22:46:17 +0800

    Update init_in_android-x86_64.sh

--- init_in_android-x86_64.sh
+++ init_in_android-x86_64.sh
@@ -1,22 +1,32 @@
 #!/system/bin/sh
-# execute "sed -i '459a init_in_android-x86_64.sh' /system/etc/init.sh" in android-x86_64-6.0-rc1-0.vdi
+# execute "sed -i '460a init_in_android-x86_64.sh' /system/etc/init.sh" in android-x86_64-6.0-rc1-0.vdi
+
+# Note: /system/xbin/quagga/sbin/init_in_android-x86_64.sh will be called 4 times in /system/etc/init.sh [init, bootcomplete, hci, hci]
+
+#--------------------------------------------------------------------
+# Note: vi /etc/preloaded-classes, #android.net.NetworkPolicyManager
+# otherwise, can't forward packages
+#--------------------------------------------------------------------
 
 # waiting a while, push init_in_android-x86_64.sh in create_vm(),
 # due to that init_in_android-x86_64.sh may be exist in android-x86_64-6.0-rc1-[1-252].vdi
 # if create android-x86_64-6.0-rc1-[1-252].vdi from scratch create, then can delete the following line. 
 
-#sleep 30
+#sleep 15 
 
 #stop netd
 
 ipadd=`ifconfig eth0 |awk -F '[ :]+' 'NR==2 {print $4}'`
 #echo ${ipadd}
 
-# Note: 112.26.2.[1-254] for android, 112.26.1.[1-254] for docker(centos)
-# must to change the IP address for every Android
-
 if [ "${ipadd}" != "112.26.2.5" ]; then
 
+	#sleep 30
+
+	#log.sh 1
+
+	# Note: 112.26.2.[1-254] for android, 112.26.1.[1-254] for docker(centos)
+	# must to change the IP address for every Android
 	# ifconfig eth0 down
 	ifconfig eth0 112.26.2.5 netmask 255.255.0.0 up
 
@@ -29,17 +39,26 @@ if [ "${ipadd}" != "112.26.2.5" ]; then
 
 	#pkill zebra
 	#pkill ospf6d
-	echo 1 > /proc/sys/net/ipv4/ip_forward
-
-	#iptables -F
-	#iptables -F -t nat
-	#iptables -F -t mangle
-	#svc wifi disable
-	#svc data disable
+	#echo 1 > /proc/sys/net/ipv4/ip_forward
 
 	#sleep 1
 	/system/xbin/quagga/sbin/zebra -d
 	/system/xbin/quagga/sbin/ospf6d -d
 fi
 
-#log.sh 1
+iptables -F
+iptables -X
+
+#iptables -F -t nat
+#iptables -X -t nat
+
+#iptables -F -t mangle
+#iptables -X -t mangle
+
+#svc wifi disable
+#svc data disable
+
+#log.sh 2
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
